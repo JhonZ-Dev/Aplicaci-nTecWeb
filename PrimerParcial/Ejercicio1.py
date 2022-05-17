921678d170713d159108ccffd2740664280e1bf5
Jhon Zambrano <jkzambrano2@espe.edu.ec>
2022-05-16 19:18:10 -0500

    PrimeraOpcion

--- PrimerParcial/Ejercicio1.py
+++ PrimerParcial/Ejercicio1.py
@@ -1,1 +1,9 @@
 #Primer Ejercicio
+print("""
+[1] Dias a Años
+[2] Semana a Dias""")
+opciones = input("Estimado usuario eliga una opcion:    ")
+if opciones == '1':
+    dias = int(input("Digite el numero de dias que va a transformar a años: "))
+    año = dias * (1 / 365)
+    print("El numeros de años es :  {}".format(año))
