44053d41621603aa8ab76268de3491452e469e23
Jhon Zambrano <jkzambrano2@espe.edu.ec>
2022-05-16 20:03:42 -0500

    Ejecicio2_ComprensionProblema

---
+++ PrimerParcial/Ejercicio2.py
@@ -0,0 +1,8 @@
+#Calcular la formula de la velocidad.
+#Primero tenemos que tener en cuenta que existes dos movimientos difentes
+#Movimiento Rectilinio Uniforme [MRU]
+#Movimiento Rectilinio Uniformemente Variado [MRUV]
+#Tendremos en cuenta las fórmulas a usar:
+#En MRU = V = d / t 
+#En MRUV = Vf = Vo + a * t
+#---------Desde Aqui va código---------------
