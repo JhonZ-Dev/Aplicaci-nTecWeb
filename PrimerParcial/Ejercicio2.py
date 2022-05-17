1ed6813597fc2e9696fa9ad394363e8d33f300ed
Jhon Zambrano <jkzambrano2@espe.edu.ec>
2022-05-16 20:12:19 -0500

    OpcionUno_terminado

--- PrimerParcial/Ejercicio2.py
+++ PrimerParcial/Ejercicio2.py
@@ -6,3 +6,22 @@
 #En MRU = V = d / t 
 #En MRUV = Vf = Vo + a * t
 #---------Desde Aqui va código---------------
+
+#Printiamos las opcines a escoger.
+print("""Estimado usuario existen dos tipo de velocidad, velocidad en MRU(Movimiento Rectilinio Uniforme variado
+        y la velocidad en movimiento rectilinio uniformente variado)
+        Elige la opción desee.
+          [1] Velocidad en MRU
+          [2] Velocidad en MRUV""")
+
+# Aqui creamos la variable para que el usuario escoga una opcion
+opciones = input("Digite una opción del 1 al 2: ")
+#Usaremos condicional if
+#Si opcines = 1, el usuario digita distancia y tiempo.
+if opciones =='2':
+    #distancia, representa el dato que el usuario ingresará que puede ser un vide flotante o entero ya que si hay distancia de 4.7m
+    distancia = float(input("Digite el valor de la distancia:   "))
+    #tiempo, de valor será entero o flotante ya que en fisica si hay tiempo de 8.9 segundos
+    tiempo = float(input("Digite el valor del tiempo:     "))
+    #Creamos el sistema o la ecuación a resolver el problema.
+    velocidad = distancia / tiempo #Formula v= d/t
